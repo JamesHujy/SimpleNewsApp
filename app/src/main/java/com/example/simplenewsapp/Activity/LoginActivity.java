3b7fd96fee3e1143ddc2215cde79e5cc848d86a8
liangnn17 <40016193+liangnn17@users.noreply.github.com>
2019-09-08 09:13:30 +0800

    Merge branch 'master' of F:\CodingFile\Android\SimpleNewsAppVersionControl with conflicts.

--- app/src/main/java/com/example/simplenewsapp/Activity/LoginActivity.java
+++ app/src/main/java/com/example/simplenewsapp/Activity/LoginActivity.java
@@ -24,7 +24,6 @@ public class LoginActivity extends Activity
     private ImageView login_head;
 
     private SQLiteOpenHelper dbHelper;
-
     @Override
     public void onCreate(Bundle savedInstanceState)
     {
